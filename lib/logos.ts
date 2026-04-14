4a68ba6b8190460729731b768bb4492b804a5ef5
CP-Zav <zavell.mercer@proton.me>
2026-04-15 06:08:16 +1000

    feat(brand): add centralized LOGOS registry for Signal, Matrix, ASA

---
+++ lib/logos.ts
@@ -0,0 +1,14 @@
+export const LOGOS = {
+  signal: {
+    icon: "/logos/signal-icon.svg",
+    wordmark: "/logos/signal-wordmark.svg",
+  },
+  matrix: {
+    icon: "/logos/matrix-icon.svg",
+    wordmark: "/logos/matrix-wordmark.svg",
+  },
+  asa: {
+    icon: "/logos/asa-icon.svg",
+    wordmark: "/logos/asa-wordmark.svg",
+  },
+} as const;
