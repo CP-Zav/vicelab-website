9d25dc8a5b68c84be94ee4b346cb0c6e94bee8c9
CP-Zav <zavell.mercer@proton.me>
2026-04-15 06:37:43 +1000

    feat(orchestration): add barrel index

---
+++ lib/orchestration/index.ts
@@ -0,0 +1,3 @@
+export { orchestrate, JOB_QUEUE_HOOK, CACHE_HOOK } from './orchestrator';
+export { TRACKER_ADAPTER, appendLog, getLogs, getJob } from './tracker';
+export type { Job, JobStatus, ExecutionLog, OrchestrationResult } from './types';
